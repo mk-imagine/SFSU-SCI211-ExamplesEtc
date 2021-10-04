d115faa9b6e21e09ab81efb6776e2319a32286ed
Tunni Chiang <tchiang3@mail.sfsu.edu>
2021-10-04 15:54:04 -0700

    added math operations class and greetings for Week 7

---
+++ src/Greetings.java
@@ -0,0 +1,13 @@
+public class Greetings {
+    public static void main (String[] args) {
+        System.out.println(christmas("Claire"));
+    }
+
+    public static String christmas (String name) {
+        return "Merry Christmas, " + name;
+    }
+
+    public static String birthday (String name) {
+        return "Happy Birday, " + name;
+    }
+}
