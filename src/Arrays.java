557cb8c7e661838f01296ab33b05cc048e98fe24
Mark Kim <mark@MacMini.local>
2021-10-15 15:41:23 -0700

    Added Arrays class for giving examples to Students

---
+++ src/Arrays.java
@@ -0,0 +1,16 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int evenCounter = 0;
+        int counter = 0;
+        while (evenCounter < 10) {
+            int rand = (int) (Math.random() * 100);
+//            System.out.println(rand*2);
+            if (rand % 2 == 0) {
+                System.out.println(rand);
+                evenCounter++;
+            }
+            counter++;
+        }
+        System.out.println("The loop iterated this many times: " + counter);
+    }
+}
