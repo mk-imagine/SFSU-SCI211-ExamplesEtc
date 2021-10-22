33956d65e6677a3e2c211c95a48aec77715ebf0b
Tunni Chiang <tchiang3@mail.sfsu.edu>
2021-10-22 13:33:24 -0700

    added isEven and testing code

--- src/Week10.java
+++ src/Week10.java
@@ -1,17 +1,22 @@
 public class Week10 {
     public static void main(String[] args) {
-        System.out.println(add(1.25, 2.45));
+        System.out.println("Testing add(1.25, 2.45): " + add(1.25, 2.45));
+        System.out.print("Testing greeting(\"Tunni\", \"Chiang\"): ");
         greeting("Tunni", "Chiang");
+        System.out.println("Testing isEven(8): " + isEven(8));
+        System.out.println("Testing isEven(9): " + isEven(9));
     }
 
     public static double add (double num1, double num2) {
-        double result = 0;
-        result = num1 + num2;
-        return result;
+        return num1 + num2;
     }
 
     public static void greeting(String firstName, String lastName) {
         System.out.println("Hello, " + firstName + " " + lastName);
+    }
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
     }
 
 }
