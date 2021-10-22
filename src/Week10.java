47b47e9b59cb4eb806135234325a71095ca50b14
Tunni Chiang <tchiang3@mail.sfsu.edu>
2021-10-22 12:48:16 -0700

    added first problem

--- src/Week10.java
+++ src/Week10.java
@@ -1,5 +1,12 @@
 public class Week10 {
     public static void main(String[] args) {
-
+        System.out.println(add(1.25, 2.45));
     }
+
+    public static double add (double num1, double num2) {
+        double result = 0;
+        result = num1 + num2;
+        return result;
+    }
+
 }
