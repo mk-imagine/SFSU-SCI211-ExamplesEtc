fb6bec446999f2397bd9536ab33c91f48330dbb5
Tunni Chiang <tchiang3@mail.sfsu.edu>
2021-10-22 13:28:18 -0700

    added greeting method

--- src/Week10.java
+++ src/Week10.java
@@ -1,12 +1,17 @@
 public class Week10 {
     public static void main(String[] args) {
         System.out.println(add(1.25, 2.45));
+        greeting("Tunni", "Chiang");
     }
 
     public static double add (double num1, double num2) {
         double result = 0;
         result = num1 + num2;
         return result;
+    }
+
+    public static void greeting(String firstName, String lastName) {
+        System.out.println("Hello, " + firstName + " " + lastName);
     }
 
 }
