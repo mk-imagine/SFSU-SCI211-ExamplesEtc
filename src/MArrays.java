771f5e40a16912fc1752652dccbbe947a9f8d6fd
Tunni Chiang <tchiang3@mail.sfsu.edu>
2021-10-29 13:37:33 -0700

    added printArray method name

--- src/MArrays.java
+++ src/MArrays.java
@@ -2,4 +2,14 @@ public class MArrays {
     public static void init2dArray(int m, int n) {
         for (int i = 0; i < size);
     }
+
+    public static void printArray(int[][] array) {
+        System.out.println("---Printing out the array ---");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+
+            }
+
+        }
+    }
 }
