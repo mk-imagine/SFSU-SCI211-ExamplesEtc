0fd3f95491e22b974a1934ca4c090f25a16f5e95
Tunni Chiang <tchiang3@mail.sfsu.edu>
2021-10-29 14:00:07 -0700

    finished the addCloumns

--- src/MArrays.java
+++ src/MArrays.java
@@ -3,6 +3,7 @@ public class MArrays {
         int[][] arr = init2dArray(10, 10);
         System.out.println("------- Printing out the array -------");
         printArray(arr);
+        addColumns(arr);
     }
 
     public static int[][] init2dArray(int m, int n) {
@@ -16,11 +17,29 @@ public class MArrays {
     }
 
     public static void printArray(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[0].length; j++) {
-                System.out.print(array[i][j] + "\t");
+        for (int rows = 0; rows < array.length; rows++) {
+            for (int columns = 0; columns < array[0].length; columns++) {
+                System.out.print(array[rows][columns] + "\t");
             }
             System.out.println();
         }
+    }
+
+    public static int[] addColumns(int[][] array) {
+        int[] results = new int[10];
+        for (int i = 0; i < results.length; i++) {
+            results[i] = 0;
+        }
+        for (int columns = 0; columns < array.length; columns++) {
+            for (int rows = 0; rows < array[0].length; rows++) {
+                results[rows] += array[columns][rows];
+            }
+        }
+        System.out.println("------- show the results -------");
+        for (int i = 0; i < results.length; i++) {
+            System.out.print(results[i] + " ");
+        }
+        System.out.println();
+        return results;
     }
 }
