746302e5edd2ecb7110a06ea4be9fdbee2d348a6
Mark Kim <mkim22@mail.sfsu.edu>
2022-09-28 18:11:09 -0700

    Arrays exercise skeleton add

---
+++ src/Stuff/Arrays1ExerciseSkeleton.java
@@ -0,0 +1,78 @@
+package Stuff;
+/**
+ * SCI 211 Arrays Exercise
+ *
+ * @author Mark Kim
+ *
+ */
+
+public class Arrays1Exercise{
+
+    public static void main(String[] args){
+        // Call the methods you implemented below
+
+        // Print statements to display results to the console
+        System.out.println("Sum of array: " + );
+        System.out.println("Avg of array: " + );
+        System.out.println("Max element of array: " + );
+        System.out.println("Index of first occurence of max element: "
+                + );
+    }
+
+    /**
+     * initRandArray initializes and returns an array with random integers
+     *
+     * @param size      Size of the array to be initialized
+     * @param min       Lower bound of random numbers
+     * @param max       Upper bound of random numbers
+     * @return arr      Returns the array of random integers
+     */
+    public static int[] initRandArray(int size, int min, int max){
+        
+    }
+
+    /**
+     * dispIntArray prints the contents of the array passed into it to the console/terminal
+     *
+     * @param arr       Array to be printed to console
+     */
+    public static void dispIntArray(int[] arr){
+        
+    }
+
+    /**
+     * sumArray sums up all the elements of the array passed into it and returns
+     * the sum
+     *
+     * @param arr       Array to be summed up
+     * @return sum      Returns the integer sum of the array
+     */
+    public static int sumArray(int[] arr){
+        
+    }
+
+    /**
+     * maxElement finds the maximum element in the array passed into it and
+     * returns the maximum element
+     *
+     * @param arr       Array to be processed
+     * @return maxNum   Returns the largest integer value in the array
+     */
+    public static int maxElement(int[] arr){
+        
+    }
+
+    /**
+     * firstIndexOfElement returns the index of an integer (the first parameter) 
+     * inside of the array passed in. If the array finds a match, it returns the 
+     * index of the match.  If no match is found, it returns -1.  Challenge:
+     * can you return the index of the LAST match?
+     *
+     * @param num       Number to be found in array
+     * @param arr       Array to be searched
+     * @return          Returns the index if the number is found, otherwise it returns -1
+     */
+    public static int firstIndexOfElement(int num, int[] arr){
+        
+    }
+}
